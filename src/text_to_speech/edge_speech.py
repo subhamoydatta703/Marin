b7fbad939654404eea459b5edc77c89826faf2b1
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-09-16 10:34:21 +0530

    Implement  text to speech codebase in python

---
+++ src/text_to_speech/edge_speech.py
@@ -0,0 +1,38 @@
+import asyncio
+import subprocess
+import edge_tts
+
+
+async def speak(text):
+    # create Edge TTS
+    communicate = edge_tts.Communicate(text,"en-IN-NeerjaNeural",rate="-10%")
+
+    # start ffplay
+    process = subprocess.Popen(
+        [
+            "ffplay",
+            "-nodisp",
+            "-autoexit",
+            "-loglevel", "quiet",
+            "-i", "pipe:0"
+        ],
+        stdin=subprocess.PIPE
+    )
+
+    # get audio chunks from Edge TTS
+    async for chunk in communicate.stream():
+        if chunk["type"] == "audio":
+            # send audio to ffplay
+            process.stdin.write(chunk["data"])
+            process.stdin.flush()
+
+    # tell ffplay there is no more data
+    process.stdin.close()
+
+    # wait for ffplay to finish
+    process.wait()
+
+
+asyncio.run(
+    speak("Hey! How are you?")
+)
---
+++ src/text_to_speech/koroko_tts.py
@@ -0,0 +1,16 @@
+# from kokoro import KPipeline
+# from rich import print
+# import torch
+
+# generator = KPipeline(lang_code='a')
+# def textGeneration(text):
+#     audio = generator(text, voice='af_heart')
+#     for i, (gs, ps, audio) in enumerate(generator):
+#     # print(i, gs, ps)
+#     print(audio(data=audio, rate=24000, autoplay=i==0))
+#     sf.write(f'{i}.wav', audio, 24000)
+    
+    
+#     # Export to WAV
+        
+    
